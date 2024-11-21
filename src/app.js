4a3a65b068a564eb0dceac18a78537c3f46bc9b3
Einar <einarescobarclavijo@gmail.com>
2024-11-21 19:12:45 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let generateCard = document.querySelector("#cardGenerator");
+  generateCard.addEventListener("click", cartas);
+  function cartas() {
+    const simboloList = ["♥", "♠", "♦", "♣"];
+    const valorList = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomSuit =
+      simboloList[Math.floor(Math.random() * simboloList.length)];
+    let randomNumber = valorList[Math.floor(Math.random() * valorList.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    document.querySelector("#simboloSup").innerHTML = randomSuit;
+    document.querySelector("#valor").innerHTML = randomNumber;
+    document.querySelector("#simboloInf").innerHTML = randomSuit;
+  }
+
+  cartas();
 };
